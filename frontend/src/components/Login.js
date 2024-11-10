e476c0d9d503dfc95b3e664c4143f4614cfb61d6
Asasinxd <peter.saletra@gmail.com>
2024-11-10 14:25:54 +0100

    Added register endpoitn

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
 
     try {
       const response = await axios.post('http://localhost:8000/api/login', {
-          username: formData.username,
+          email: formData.username,
           password: formData.password
       });
 
