15c5fef54b3a40c1c496abe3a45b73a1a88ec294
Peter <peter.saletra@gmail.com>
2024-12-29 23:59:34 +0100

    Added endpoint for getting games based on event

--- backend/api/services/games.py
+++ backend/api/services/games.py
@@ -1,4 +1,5 @@
 from api.database.models.games import Games, GameStatus
+from api.database.models.odds import Odds, OddsType
 from typing_extensions import Optional
 from sqlalchemy.orm import Session
 from api.core.logging import logger
@@ -15,6 +16,41 @@ def get_games_categories(db: Session) -> Optional[dict]:
             categories[sport_type].append(event_name)
 
         return categories
+    except Exception as e:
+        logger.error(e)
+        return None
+
+def get_games_by_event_name(event_name: str, db: Session) -> Optional[dict]:
+    try:
+        result = db.query(Games.home, Games.away, Games.start_time, Odds.odds, Odds.odds_type).join(Odds).filter(Games.event_name == event_name and Games.game_status != GameStatus.FINISHED).all()
+
+        grouped_games = {}
+
+        for home, away, start_time, odds, odds_type in result:
+            game_key = (home, away, start_time)
+
+            if game_key not in grouped_games:
+                grouped_games[game_key] = {
+                    'home': home,
+                    'away': away,
+                    'start_time': start_time.time(),
+                    'start_date': start_time.date(),
+                    'odds1': None,
+                    'oddsX': None,
+                    'odds2': None
+                }
+
+            if odds_type == OddsType.One:
+                grouped_games[game_key]['odds1'] = odds
+            elif odds_type == OddsType.Two:
+                grouped_games[game_key]['odds2'] = odds
+            elif odds_type == OddsType.X:
+                grouped_games[game_key]['oddsX'] = odds
+
+        games = {str(i): game_data for i, game_data in enumerate(grouped_games.values())}
+
+        return games
+
     except Exception as e:
         logger.error(e)
         return None
