e5daf29211b80607eeaa3eba8b4ef2fe5854ed72
VoxeveR <piotrzietek01@gmail.com>
2025-01-02 17:20:22 +0100

    betbanner - with loaded data from api

--- frontend/src/components/BetBanner.js
+++ frontend/src/components/BetBanner.js
@@ -1,33 +1,63 @@
 import React from 'react';
-import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { ToggleButton, ToggleButtonGroup, Button } from "react-bootstrap";
 
-const BetBanner = ({ betData, onBetSelect }) => {
+// Replace betIndex with betID
+const BetBanner = ({ betData = {}, betIndex, onBetSelect }) => {
+    const {
+        start_date = '',
+        start_time = '',
+        home = '',
+        away = '',
+        odds1 = '-',
+        oddsX = '-',
+        odds2 = '-'
+    } = betData || {};
+    const savedValue = sessionStorage.getItem(`${betIndex}`); // Loading selectionData
+
+    //FIXME:
+    // example function checking if it works
+    const handleDeselect = () => {
+        sessionStorage.removeItem(`${betIndex}`);
+
+        onBetSelect(null);
+    };
+
     return (
-        <div className="container bg-body-secondary">
+        <div className="container bg-body-secondary mt-2">
             <div className="row align-items-center m-0">
                 <div className="col">
-
-                    <p className="mb-0"><small className="fs-6"> {betData.date} </small><b> {betData.team1} </b>
-                        <small> {betData.hour} </small> <b> {betData.team2} </b></p>
+                    <small className="fs-6"> {start_date} </small>
+                    <p className="mb-0"><b> {home} </b>
+                        <small> {start_time.slice(0, start_time.length - 3)} </small> <b> {away} </b></p>
                 </div>
                 <div className="col ms-auto text-end">
-                    <ToggleButtonGroup type="radio" name="betType">
+                    <ToggleButtonGroup type="radio" name={betIndex} value={savedValue}>
                         <ToggleButton
-                            id="1."
-                            value={'1'}
-                            onClick = {() => onBetSelect("1")}>
-                            1.</ToggleButton>
+                            id={`1:${betIndex}`}
+                            value={`1`}
+                            onClick={() => onBetSelect(`${betIndex}:1`)}
+                        >
+                            1.
+                        </ToggleButton>
                         <ToggleButton
-                            id="X"
-                            value={'X'}
-                            onClick = {() => onBetSelect("X")}>
-                            X</ToggleButton>
+                            id={`X:${betIndex}`}
+                            value={`X`}
+                            onClick={() => onBetSelect(`${betIndex}:X`)}
+                        >
+                            X
+                        </ToggleButton>
                         <ToggleButton
-                            id="2."
-                            value={'2'}
-                            onClick = {() => onBetSelect("2")}>
-                            2.</ToggleButton>
+                            id={`2:${betIndex}`}
+                            value={`2`}
+                            onClick={() => onBetSelect(`${betIndex}:2`)}
+                        >
+                            2.
+                        </ToggleButton>
                     </ToggleButtonGroup>
+
+                    <Button variant="secondary" onClick={() => handleDeselect()} className="mt-2">
+                        Deselect
+                    </Button>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Bets.js
+++ frontend/src/pages/Bets.js
@@ -7,27 +7,18 @@ import axios from 'axios';
 import {Accordion, ListGroup} from "react-bootstrap";
 
 function Bets(){
+    const [selectedCategory, setSelectedCategory] = useState('');
     const [categoryList, setCategoryList] = useState([]);
-    const [eventList, setEventList] = useState(["MAJOR", "LEC", "LA Liga"]);
-    const [betsList, setBetsList] = useState({
-        category:"League of Legends",
-        team1:"T1",
-        team1stake:"1.70",
-        team2:"G2",
-        team2stake:"2.70",
-        drawstake:"4.70",
-        date:"22.12",
-        hour:"17:00"
-    });
+    const [betsList, setBetsList] = useState({});
 
-    const [category, setCategory] = useState('');
+
     const [selectedBets, setSelectedBets] = useState([]);
 
-    const content = category ? (
+    const content = selectedCategory ? (
         <div className="p-3 bg-light border rounded mt-2">
-            <h5>Details for {category}</h5>
+            <h5>Details for {selectedCategory}</h5>
             <p>
-                Here are some example details for <b>{category}</b>.
+                Here are some example details for <b>{selectedCategory}</b>.
             </p>
         </div>
     ) : (
@@ -37,8 +28,10 @@ function Bets(){
     useEffect(()=>{
             //handling filling bets -> API CALL
             try{
-                axios.get('http://localhost:8000/api/games/categories').
-                then((response) => {
+                axios.get('http://localhost:8000/api/games/categories')
+                    .then((response) => {
+                    //console.log(response.data);
+                    //console.log(response.data.data);
                     setCategoryList(response.data.data);
                 });
             } catch(error){
@@ -46,15 +39,51 @@ function Bets(){
             }
     }, []);
 
+
     function handleCategory(event) {
-        setCategory(event.target.textContent);
+        const selectedCategory = event.target.textContent;
+        setSelectedCategory(selectedCategory);
+
+        const gameURL = `http://localhost:8000/api/games/${selectedCategory}`;
+
+        try {
+            axios.get(gameURL)
+                .then((response) => {
+                    setBetsList(response.data.data);
+                    console.log(response.data.data);
+                    //console.log(typeof betsList);
+                })
+        } catch(error){
+            console.log(error.response.data);
+        }
     }
 
-    function handleBetSelection(type) {
-        const newSelectedBets = {
-            ...selectedBets,
+    function handleBetSelection(selection) {
+        if (selection) {
+            setSelectedBets(s => [...s, selection]);
+
+            // adding selectedBets list to browser memory
+            sessionStorage.setItem('selectedBets', JSON.stringify(selectedBets));
+
+            // adding betStatus to browser memory -> maybe should remove
+            const selectionData = selection.split(":");
+            sessionStorage.setItem(`${selectionData[0]}`, selectionData[1]);
+            console.log(betsList);
+
+            //saving whole bet data - user selection - xD
+            sessionStorage.setItem(`${selectionData[0]}_DATA`, JSON.stringify(betsList[selectionData[0]]));
+            console.log(sessionStorage.getItem(`${selectionData[0]}`)); // Logs the stored selection
+        } else {
+            //FIXME:
+            // NOT HANDLED PROPERLY
+
+            // Handle deselect scenario: Reset or remove from selectedBets
+            setSelectedBets(s => s.filter(item => item !== selection)); // Removes the deselected bet
+            sessionStorage.removeItem(selection); // Remove the deselected bet from sessionStorage
+            console.log("Deselected: No bet selected.");
         }
-       // setSelectedBets(..prev, event.target.textContent);
+
+        console.log("Current selected bets:", selectedBets);
     }
 
     return (
@@ -70,12 +99,14 @@ function Bets(){
                                     <Accordion.Item
                                         eventKey={index}
                                         // border1 d-flex align-items-center
-                                        onClick={handleCategory}>
+                                        >
                                         <Accordion.Header>{category}</Accordion.Header>
                                         <Accordion.Body>
                                             <ListGroup>
                                                 {categoryList[category].map((item, index) => (
-                                                    <ListGroup.Item key={index}>{item}</ListGroup.Item>
+                                                    <ListGroup.Item key={index}
+                                                                    onClick={handleCategory}
+                                                                    >{item}</ListGroup.Item>
                                                 ))}
                                             </ListGroup>
                                         </Accordion.Body>
@@ -86,7 +117,14 @@ function Bets(){
                     </div>
                     <div className="col-6 custom-second-column h-100 bg-white">
                         <p>{content}</p>
-                        <BetBanner betData={betsList} onBetSelect={handleBetSelection} />
+                        {Object.entries(betsList).map(([key, betData]) => (
+                            <BetBanner
+                                betData={betData}
+                                key={key}
+                                betIndex={key}
+                                onBetSelect={handleBetSelection}
+                            />
+                        ))}
                     </div>
                     <div className="col bg-primary-subtle">
 
