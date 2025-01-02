f8fafef5c41c94c7c6d51adf2f557987699d0296
Asasinxd <peter.saletra@gmail.com>
2025-01-02 14:38:10 +0100

    Updated schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,6 @@
 -- DROPPING TABLES
 
+DROP TABLE IF EXISTS BetsGames;
 DROP TABLE IF EXISTS Bets;
 DROP TABLE IF EXISTS Employees;
 DROP TABLE IF EXISTS Odds;
@@ -7,6 +8,7 @@ DROP TABLE IF EXISTS User_roles;
 DROP TABLE IF EXISTS Transactions;
 DROP TABLE IF EXISTS Promotions;
 DROP TABLE IF EXISTS Odds;
+DROP TABLE IF EXISTS Account;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Games;
 DROP TYPE IF EXISTS role;
@@ -23,89 +25,122 @@ DROP TYPE IF EXISTS positionTypes;
 -- CREATING TABLE STRUCTURE
 
 create table Users(
-    user_id serial NOT NULL PRIMARY KEY,
-    username VARCHAR(40) NOT NULL,
-    password TEXT NOT NULL,
-    name VARCHAR(40) NOT NULL,
-    surname VARCHAR(60) NOT NULL,
-    email TEXT NOT NULL,
-    PESEL numeric(11),
-    id_number VARCHAR(10),
-    phone_number numeric(11),
-    is_verified BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                      user_id serial NOT NULL PRIMARY KEY,
+                      username VARCHAR(40) NOT NULL,
+                      password TEXT NOT NULL,
+                      name VARCHAR(40) NOT NULL,
+                      surname VARCHAR(60) NOT NULL,
+                      email TEXT NOT NULL,
+                      PESEL numeric(11),
+                      id_number VARCHAR(10),
+                      phone_number numeric(11),
+                      is_verified BOOLEAN,
+                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE type role as enum('USER', 'ADMIN', 'ANALYST');
 
 create table User_roles(
-    role_id serial NOT NULL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES Users,
-    role_name role
+                           role_id serial NOT NULL PRIMARY KEY,
+                           user_id integer NOT NULL REFERENCES Users,
+                           role_name role
 );
+
+
+CREATE table Account(
+        account_id serial NOT NULL PRIMARY KEY,
+        user_id integer NOT NULL REFERENCES Users,
+        balance decimal(15, 2) NOT NULL DEFAULT 0.00
+);
+
 
 CREATE type transactionType as enum('');
 CREATE type transactionStatus as enum();
 
 
 create table Transactions(
-    transaction_id serial NOT NULL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES Users,
-    transaction_type transactionType NOT NULL,
-    amount decimal(12, 2) NOT NULL,
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status transactionType NOT NULL
+                             transaction_id serial NOT NULL PRIMARY KEY,
+                             account integer NOT NULL REFERENCES Account,
+                             transaction_type transactionType NOT NULL,
+                             amount decimal(12, 2) NOT NULL,
+                             transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                             status transactionType NOT NULL
 );
 
 create type gameStatus as enum('BEFORE', 'PLAYING', 'FINISHED');
 create type sportType as enum('FOOTBALL', 'BASKETBALL', 'LEAGUE_OF_LEGENDS', 'CS_GO', 'BOXING', 'MMA');
 
 create table Games(
-    game_id serial NOT NULL PRIMARY KEY,
-    home VARCHAR(100) NOT NULL,
-    away VARCHAR(100) NOT NULL,
-    event_name VARCHAR(100) NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-    game_status gameStatus NOT NULL,
-    sport_type sportType NOT NULL
+                      game_id serial NOT NULL PRIMARY KEY,
+                      home VARCHAR(100) NOT NULL,
+                      away VARCHAR(100) NOT NULL,
+                      event_name VARCHAR(100) NOT NULL,
+                      start_time TIMESTAMP NOT NULL,
+                      game_status gameStatus NOT NULL,
+                      sport_type sportType NOT NULL
 );
 
 create type betStatusType as enum('SETTLED', 'UNSETTLED');
 create type betResult  as enum('WON', 'LOST');
 
 create table Bets(
-    bet_id serial not null PRIMARY KEY,
-    user_id integer not null REFERENCES Users,
-    game_id integer not null REFERENCES Games,
-    bet_amount decimal(12, 2) NOT NULL,
-    odds decimal(2, 2) NOT NULL,
-    status BetStatusType NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    bet_result betResult
+                     bet_id serial not null PRIMARY KEY,
+                     user_id integer not null REFERENCES Users,
+                     bet_amount decimal(12, 2) NOT NULL,
+                     odds decimal(2, 2) NOT NULL,
+                     status BetStatusType NOT NULL,
+                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                     bet_result betResult
+);
+
+create table BetsGames(
+    bet_id integer NOT NULL REFERENCES Bets,
+    game_id integer NOT NULL REFERENCES Games,
+    PRIMARY KEY (bet_id, game_id)
 );
 
 create type oddsType as enum('One', 'X', 'Two');
 
 create table Odds(
-    odds_id serial NOT NULL PRIMARY KEY,
-    game_id integer NOT NULL REFERENCES Games,
-    odds int NOT NULL,
-    odds_type oddsType NOT NULL
+                     odds_id serial NOT NULL PRIMARY KEY,
+                     game_id integer NOT NULL REFERENCES Games,
+                     odds int NOT NULL,
+                     odds_type oddsType NOT NULL
 );
 
 create type promotionType as enum('PLACEHOLDER');
 
 create table Promotions(
-    promotion_id serial NOT NULL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES Users,
-    promotion_name VARCHAR(200) NOT NULL,
-    promotion_type promotionType NOT NULL,
-    start_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    end_date TIMESTAMP NOT NULL,
-    requirements VARCHAR(200) NOT NULL
+                           promotion_id serial NOT NULL PRIMARY KEY,
+                           user_id integer NOT NULL REFERENCES Users,
+                           promotion_name VARCHAR(200) NOT NULL,
+                           promotion_type promotionType NOT NULL,
+                           start_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           end_date TIMESTAMP NOT NULL,
+                           requirements VARCHAR(200) NOT NULL
 );
 
 -- INSERTING SAMPLE DATA
+
+-- INSERT TEST USERS
+INSERT INTO Users (username, password, name, surname, email, PESEL, id_number, phone_number, is_verified)
+VALUES
+    ('test', 'test', 'John', 'Doe', 'test_user@example.com', '12345678901', 'AB123456', '123456789', TRUE), -- Regular User
+    ('admin', 'admin', 'Jane', 'Smith', 'test_admin@example.com', '98765432109', 'CD654321', '987654321', TRUE); -- Admin User
+
+-- ASSIGN ROLES TO USERS
+INSERT INTO User_roles (user_id, role_name)
+VALUES
+    (1, 'USER'),   -- Assign 'USER' role to the first user
+    (2, 'ADMIN');  -- Assign 'ADMIN' role to the second user
+
+-- CREATE ACCOUNT FOR REGULAR USER
+INSERT INTO Account (user_id, balance)
+VALUES
+    (1, 1000.00); -- Initial balance for the regular user
+
+
+-- INSERT TEST GAMES AND ODDS
 
 INSERT INTO Games (home, away, event_name, start_time, game_status, sport_type)
 VALUES
@@ -160,5 +195,3 @@ VALUES
     (11, 1.6, 'One'),  -- Golden State Warriors win
     (11, 3.0, 'X'),    -- Draw
     (11, 2.1, 'Two');  -- Los Angeles Clippers win
-
-
