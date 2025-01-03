864c560ee6880fbdcdf323358666991075b632a3
VoxeveR <piotrzietek01@gmail.com>
2025-01-03 20:48:24 +0100

    added betbox api handling

--- frontend/src/components/BetBox.js
+++ frontend/src/components/BetBox.js
@@ -4,69 +4,34 @@ import BetBoxCard from "./BetBoxCard";
 import { FormControl, Form } from "react-bootstrap";
 import axios from "axios";
 
-function BetBox({selectedBets, handleDeleteBet}) {
-    const [betDetails, setBetDetails] = useState([]);
+function BetBox({handleDeleteBet, handleUpdateBets}) {
 
-    useEffect(()=>{
-        const storedBets = JSON.parse(sessionStorage.getItem('selectedBets'));
-        console.log("testing useEffect", storedBets);
+    const storedBets = JSON.parse(sessionStorage.getItem('selectedBets')) || [];
 
-        if(storedBets != null){
-            let currentBet = [];
-            for(let i = 0; i < storedBets.length; i++){
-                const betData = JSON.parse(sessionStorage.getItem(`${storedBets[i]}_DATA`))
-
-                currentBet.push({
-                    ...betData,
-                    betID: (storedBets[i]),
-                    userSelection: sessionStorage.getItem(storedBets[i])
-                 });
-            }
-            setBetDetails(currentBet);
-
-            console.log("bets", betDetails);
-        }
-
-    }, [selectedBets]);
-
-
+    const currentBetDetails = storedBets.map(betId => ({
+        ...JSON.parse(sessionStorage.getItem(`${betId}_DATA`)),
+        betID: betId,
+        userSelection: sessionStorage.getItem(betId)
+    }));
 
     function onDeleteBet(betID){
-        /*
-        setBetDetails(prevBets => prevBets.filter(bet => bet.betID !== betID));
-
-        const updatedBets = selectedBets.filter(bet => bet !== betID);
-        sessionStorage.setItem('selectedBets', JSON.stringify(updatedBets));
-
-        sessionStorage.removeItem(betID);
-        sessionStorage.removeItem(`${betID}_DATA`);
-        */
-
         handleDeleteBet(betID);
     }
 
     function handleMakeBet(event){
         event.preventDefault();
-        console.log(event.target.stake.value);
+
         const userID = sessionStorage.getItem('userID');
         const stake = event.target.stake.value;
-        /*
-                const games = betDetails.reduce((acc, bet) => {
-                    acc[`'${bet.betID}'`] = `${bet.userSelection}`;
-                    return acc;
-                }, {});
-                */
 
-        const games = betDetails.reduce((acc, bet) => {
+        const games = currentBetDetails.reduce((acc, bet) => {
              acc[bet.betID] = bet.userSelection;
              return acc;
         }, {});
 
-        console.log(typeof games);
-        console.log(typeof games[1]);
 
         let odds = 1;
-        betDetails.forEach(value => {
+        currentBetDetails.forEach(value => {
             if(value.userSelection === '1'){
                 odds *= value.odds1;
             } else if (value.userSelection === '2'){
@@ -75,21 +40,17 @@ function BetBox({selectedBets, handleDeleteBet}) {
                 odds *= value.oddsX;
             }
         })
-        console.log(games);
-        console.log(odds);
-        console.log(typeof userID);
 
         const data = {
             user_id: Number(userID),
             games: games,
-            amount: Number(stake),
-            odds: Number(odds)
+            amount: parseFloat(stake),
+            odds: parseFloat(odds)
         }
-
-        console.log(data);
 
         axios.post("http://localhost:8000/api/bets/", data).then(res => {
             console.log(res.data);
+            handleUpdateBets();
         }).catch(err => {
             console.log(err);
         })
@@ -99,8 +60,8 @@ function BetBox({selectedBets, handleDeleteBet}) {
         <div className="bet-box p-3 bg-light border rounded mx-auto mh-100">
             <h5>Place Your Bet</h5>
             <div className="container-fluid overflow-auto bet-box-scroll">
-                {Object.entries(betDetails).map(([key, value]) => (
-                    <BetBoxCard key={key} bet = {value} handleDeleteBet={onDeleteBet}/>
+                {currentBetDetails.map((bet, index) => (
+                    <BetBoxCard key={index} bet = {bet} handleDeleteBet={onDeleteBet}/>
                 ))}
             </div>
             <div>
